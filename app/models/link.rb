604208374fd4660de8dcfc3787e7e4a5eced8ff9
alivetocode <sharman@nc.rr.com>
2016-03-11 08:48:53 -0500

    Add comments

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,5 +2,7 @@ class Link < ActiveRecord::Base
 
 acts_as_votable
 belongs_to :user
+has_many :comments
 
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,16 @@
 Rails.application.routes.draw do
+  resources :comments
+
   devise_for :users
-  resources :links do 
+  resources :links do
     member do
-      put "like", to: "links#upvote"
+      put "like", to:    "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
-  
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  root "links#index"
 
-  # You can have the root of your site routed with "root"
-  root 'links#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
