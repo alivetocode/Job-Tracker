a3aed0da84f14bcc55bd32beb4c6d8b2b24e60c0
alivetocode <sharman@nc.rr.com>
2016-03-12 15:34:01 -0500

    Add vailidations to models

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,8 +4,16 @@ acts_as_votable
 belongs_to :user
 has_many :comments
 
-validates :title, presence: true, length: { in: 1..30 }
-validates :url, presence: true, :format => URI::regexp(%w(http https))
+validates :title, 
+						presence: true, 
+						length: { in: 1..30 },
+						uniqueness: true
+
+ validates :url,
+            presence: { message: "- Website address is required" }, 
+            length: { maximum: 245 },
+            format: { with: URI.regexp(%w(http https)), message: "- Please check the format of your website address. It should begin with http: or https:" }
+
 
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :links 
+  has_many  :links 
   belongs_to :user
 
   validates :name, presence: true, length: { in: 1..30 }
